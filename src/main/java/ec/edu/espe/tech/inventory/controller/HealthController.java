f039c274ddb608491b814d942f565603adf6090c
Dennis-Andrade <denisandradeg@gmail.com>
2026-05-31 09:45:56 -0500

    Creacion del proyecto Base

---
+++ src/main/java/ec/edu/espe/tech/inventory/controller/HealthController.java
@@ -0,0 +1,15 @@
+package ec.edu.espe.tech.inventory.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "ESPE-Tech Inventory activo";
+    }
+}
